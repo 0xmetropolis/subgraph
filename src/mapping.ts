67a5e9b72620392b24133922ae168389b4100c4e
Will Kim <will@sonar-labs.com>
2021-09-16 12:00:39 -0400

    fix: handle zero address transfers

--- src/mapping.ts
+++ src/mapping.ts
@@ -13,56 +13,29 @@ import { addressZero } from "../test/fixtures";
 // import { log } from "matchstick-as/assembly/log";
 // export { runTests } from "../test/integration.spec";
 
-// export function handleTransferBatch(event: TransferBatch): void {
-//   const to = event.params.to;
-//   const from = event.params.from;
-//   const ids = event.params.ids;
-
-//   let toUser = User.load(to.toHex());
-//   if (toUser === null) {
-//     toUser = new User(to.toHex());
-//   }
-
-//   // GraphQL expects ID to be a string
-//   const stringIds = ids.map(id => id.toString());
-
-//   toUser.pods = toUser.pods.concat(stringIds);
-//   toUser.save();
-
-//   // The fromUser should never be null, because the User is created
-//   // when they first receive a token.
-//   // const fromUser = User.load(from.toHex());
-//   // // This check is just assurance.
-//   // if (fromUser !== null) {
-//   //   // Remove each ID from the fromUser's pod array
-//   //   stringIds.forEach(id => {
-//   //     fromUser.pods = fromUser.pods.splice(fromUser.pods.indexOf(id.toString()), 1);
-//   //   })
-//   //   fromUser.save();
-//   // }
-// }
-
 export function handleTransferSingle(event: TransferSingle): void {
   let to = event.params.to.toHex();
   let from = event.params.from.toHex();
   let id = event.params.id.toString();
 
   // Create UserPod
-  let podUser = new PodUser(to + '-' + id);
-  podUser.user = to;
-  podUser.pod = id;
-  podUser.save();
-  
+  if (to != addressZero) {
+    let podUser = new PodUser(to + '-' + id);
+    podUser.user = to;
+    podUser.pod = id;
+    podUser.save();
+  }
+
   // Remove old one
   if (from != addressZero) {
     store.remove('PodUser', from + '-' + id);
   }
 
-  let toUser = User.load(to);
-  if (toUser === null) {
-    toUser = new User(to);
+  // Create new address, unless it's zero address
+  if (to != addressZero) {
+    let toUser = new User(to);
+    toUser.save();
   }
-  toUser.save();
 }
 
 export function handleCreateSafe(event: CreateSafe): void {
